8cb0bf563093ad0485b3ec6f69851253cdb9684d
Sheraz Ahmed <sherazahmed93@gmail.com>
2017-11-21 11:49:05 +0500

    Toolbar Set

--- app/src/main/java/com/houseofdoyens/ultrahdplayer20/MainActivity.java
+++ app/src/main/java/com/houseofdoyens/ultrahdplayer20/MainActivity.java
@@ -1,24 +1,50 @@
 package com.houseofdoyens.ultrahdplayer20;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Icon;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.joanzapata.iconify.IconDrawable;
+import com.joanzapata.iconify.Iconify;
+import com.joanzapata.iconify.fonts.EntypoIcons;
+import com.joanzapata.iconify.fonts.EntypoModule;
+import com.joanzapata.iconify.fonts.FontAwesomeIcons;
+import com.joanzapata.iconify.fonts.FontAwesomeModule;
+
 public class MainActivity extends AppCompatActivity {
+
+    IconDrawable play, settings, amp, search, hamburger ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Iconify.with(new FontAwesomeModule())
+        .with(new EntypoModule());
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        play = new IconDrawable(this, FontAwesomeIcons.fa_play).colorRes(R.color.colorWhite).actionBarSize();
+        toolbar.setLogo(play);
+
+        
+        //        Hamburger For Toolbar
+        //        hamburger = new IconDrawable(this, FontAwesomeIcons.fa_bars).colorRes(R.color.colorWhite).actionBarSize();
+        //        toolbar.setNavigationIcon(hamburger);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.recentPlay);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,8 +56,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu;
+        // this adds items to the action bar if it is present.
+        settings = new IconDrawable(this, FontAwesomeIcons.fa_cog).colorRes(R.color.colorWhite).actionBarSize();
+        amp = new IconDrawable(this, EntypoIcons.entypo_sound_mix).colorRes(R.color.colorWhite).actionBarSize();
+        search = new IconDrawable(this, FontAwesomeIcons.fa_search).colorRes(R.color.colorWhite).actionBarSize();
+
+//        getActionBar().setIcon( play );
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        menu.findItem(R.id.action_settings).setIcon(settings);
+        menu.findItem(R.id.action_amp).setIcon(amp);
+        menu.findItem(R.id.action_search).setIcon(search);
         return true;
     }
 
